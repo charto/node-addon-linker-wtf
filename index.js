85fcf36ffb38f590674fd503e888ad93a71210d0
jjrv <juha@busfaster.com>
2017-05-04 18:47:00 +0300

    Add Travis.

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ require('./build/Release/bazinga.node');
 
 var parser = new (require('node-expat').Parser)('UTF-8');
 
+var OK = false;
+
+parser.on('endElement', function() { OK = true; });
+
 parser.write('<a></a>');
+
+if(!OK) throw(new Error('WTF'));
